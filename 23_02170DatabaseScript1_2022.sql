784fc87691e1b746e35155420a41db52b669c40a
Dyberg <mathias.rerup-dyberg@hotmail.com>
2022-03-26 15:30:11 +0100

    Add initial files

---
+++ 23_02170DatabaseScript1_2022.sql
@@ -0,0 +1,3 @@
+# must contain:
+# (1) the statements used to create the database, its tables and views (as used in section 5 of the report)
+# (2) the statements used to populate the tables (as used in section 6)
---
+++ 23_02170DatabaseScript2_2022.sql
@@ -0,0 +1,4 @@
+# It must contain:
+# (1) the queries made (as in section 7)
+# (2) the delete/update statements used to change the tables (as in section 8), and
+# (3) the statements used to create and apply functions, procedures, triggers, and events (as in section 9)
