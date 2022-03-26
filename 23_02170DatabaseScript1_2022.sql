4bb9c328b76d13099cfcaade8d499eecb0f7b2ab
Dyberg <mathias.rerup-dyberg@hotmail.com>
2022-03-26 15:50:00 +0100

    temp change

--- 23_02170DatabaseScript1_2022.sql
+++ 23_02170DatabaseScript1_2022.sql
@@ -1,3 +1,4 @@
 # must contain:
 # (1) the statements used to create the database, its tables and views (as used in section 5 of the report)
 # (2) the statements used to populate the tables (as used in section 6)
+# Rasmus dum
